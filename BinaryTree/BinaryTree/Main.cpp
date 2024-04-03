2d4506293a4d58d79ccb53c2aa1018ca0a051ea5
Ivan Levitskiy <levitskiyivan3@gmail.com>
2024-04-03 18:31:46 +0300

    Finaly version of lab, and he doing perfect

--- BinaryTree/BinaryTree/Main.cpp
+++ BinaryTree/BinaryTree/Main.cpp
@@ -49,48 +49,95 @@ void showNode(node* root)
 		return;
 	}
 	showNode(root->left);
-	cout << root->num << "; ";
+	std::cout << root->num << "; ";
 	showNode(root->right);
 }
 
-bool searchNum(node* root,int number,int* level)
+bool searchNum(node* root, int number,int* level)
 {
-	if (!root)
+	
+	if (root->num == number)
 	{
-		return false;
+		return true;
 	}
 	(*level)++;
-	bool check = false;
-
-	if (root->num == number)
-		return true;
-	else
+	if (root->num > number && root->left != NULL)
 	{
-		if (root->num < number)
-		{
-			check = searchNum(root->right, number, level);
-		}
+		searchNum(root->left, number, level);
 	}
-
-	if (!check)
+	else if(root->num < number && root->right != NULL)
 	{
-		(*level)--;
-		cout << "deLevel  " << endl;
+		searchNum(root->right, number, level);
 	}
-
 }
 
+int height(node* root) 
+{
+	if (root == NULL) 
+	{
+		return 0;
+	}
+	return 1 + max(height(root->left), height(root->right));
+}
+
+void ShowNumH(node* root, int hight, int level)
+{
+	if (level > hight)
+		return;
+	if (level == hight)
+	{
+		std::cout << root->num << ", ";
+	}
+	if (root->left != NULL)
+	{
+		level++;
+		ShowNumH(root->left, hight, level);
+		level--;
+	}
+	if (root->right != NULL )
+	{
+		level++;
+		ShowNumH(root->right, hight, level);
+		level--;
+	}
+	level++;
+}
 int main()
 {
 	srand(time(NULL));
 	node* root = new node;
-	root->num = rand() % 10;
+	root->num = 7;
 	root->left = root->right = NULL;
+
+	int arr[] = {7, 13, 4, 18, 2, 11, 9, 5, 16, 3, 0, 10, 20, 8, 1, 14, 6, 17, 12, 19};
+	int level = 0;
 
 	for (int i = 0; i < 20; i++)
 	{
-		addNode(root, rand() % 10);
+		addNode(root, arr[i]);
 	}
 	showNode(root);
 
+	std::cout << endl;
+
+	std::cout << "what number you search ?" << endl;
+	int num = 0;
+	cin >> num;
+	if (searchNum(root, num, &level) == true)
+	{
+		std::cout << "Tree have you number, and his hignt = " << level << endl;
+	}
+	else
+		std::cout << "Tree dont have this number" << endl;
+	level = 0;
+
+	std::cout <<"Hight of this tree is a "<< height(root) << endl;
+	
+	std::cout << "chois hight tree" << endl;
+	int hig = 0;
+	cin >> hig;
+	std::cout << "number in hights of you want is: ";
+	ShowNumH(root, hig, 0);
+
+	return 0;
 }
